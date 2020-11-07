074b5b41a96cc9f3f23106aa45fd74db26dcfd9a
Bing Rao <bing.rao@outlook.com>
2020-11-07 18:04:43 -0500

    add status

--- src/main/scala/org/ucf/ml/utils/Vocabulary.scala
+++ src/main/scala/org/ucf/ml/utils/Vocabulary.scala
@@ -1,13 +1,10 @@
 package org.ucf.ml
 package utils
-import com.github.javaparser.StaticJavaParser
-import com.github.javaparser.printer.PrettyPrinterConfiguration
-import parser.Granularity
-
-import scala.collection.parallel._
 import net.sourceforge.argparse4j.inf.{Namespace => ConfigNamespace}
 
 import scala.collection.JavaConversions._
+import scala.collection.parallel.immutable.ParSeq
+import scala.io.Source
 
 class Vocabulary(config: ConfigNamespace) extends parser.JavaParser {
 
@@ -31,14 +28,19 @@ class Vocabulary(config: ConfigNamespace) extends parser.JavaParser {
     val min_tokens = nums_tokens.min
     val average_tokens = nums_tokens.sum / nums_tokens.size
 
-    val nums_occur = nums_tokens.map(_ / 10).groupBy(identity).mapValues(_.size)
-    logger.info(s"nums_of_code\tmax\tmin\taverage")
-    logger.info(s"${nums_tokens.size}\t${max_tokens}\t${min_tokens}\t${average_tokens}")
-    val names = nums_occur.map(e => s"[${e._1 * 10}, ${(e._1 + 1) * 10})").mkString("\t")
+    val step = (max_tokens / 20 / 10 + 1) * 10
+
+    val allTokens = data.flatMap(_.toSeq).filter(_ != "DummyClass")
+    val vocab = allTokens.groupBy(identity).mapValues(_.size).toList.sortBy(_._2)(Ordering[Int].reverse)
+
+    val nums_occur = nums_tokens.map(_ / step).groupBy(identity).mapValues(_.size).toList.sortBy(_._1)
+    logger.info(s"average, max, min, vocab_size, nums_of_code: " +
+      s"${average_tokens}\t${max_tokens}\t${min_tokens}\t${vocab.size}\t${nums_tokens.size}")
+    val names = nums_occur.map(e => s"[${e._1 * step}, ${(e._1 + 1) * step})").mkString("\t")
     val values = nums_occur.map(_._2).mkString("\t")
-    logger.info("The total number of per each counting period, and step is 10")
-    logger.info(s"\t${names}")
-    logger.info(s"value\t${values}")
+    logger.info(s"The total number of per each counting period, and step is ${step}")
+    logger.info(s"${names}")
+    logger.info(s"${values}\n")
   }
 
   def build_vocab(buggyPath: List[String],
@@ -46,21 +48,37 @@ class Vocabulary(config: ConfigNamespace) extends parser.JavaParser {
                   isFile:Boolean = false) = {
 
     val buggyTokens = buggyPath.par.map { src => {
-      val cu = getComplationUnit(src, METHOD, isFile)
-      cu.getTokenRange.get().toList.filter(
-        e => e.getText != " " && e.getText != "\n").map(_.getText)
-     }
+      val reg = try {
+        val cu = getComplationUnit(src, METHOD, isFile)
+        cu.getTokenRange.get().toList.filter(
+          e => e.getText != " " && e.getText != "\n").map(_.getText)
+      } catch {
+        case e: Exception => {
+          readSourceCode(src, isFile).split(" ").toList
+        }
+      }
+      reg.asInstanceOf[List[String]]
     }
-    logger.info(s"[Buggy] Average")
+    }
+
+    logger.info(s"The statistics of buggy code: ")
     get_statistics(buggyTokens)
 
     val fixedTokens = fixedPath.par.map { src => {
-      val cu = getComplationUnit(src, METHOD, isFile)
-      cu.getTokenRange.get().toList.filter(
-        e => e.getText != " " && e.getText != "\n").map(_.getText)
+      val reg = try {
+        val cu = getComplationUnit(src, METHOD, isFile)
+        cu.getTokenRange.get().toList.filter(
+          e => e.getText != " " && e.getText != "\n").map(_.getText)
+      } catch {
+        case e: Exception => {
+          readSourceCode(src, isFile).split(" ").toList
+        }
+      }
+      reg.asInstanceOf[List[String]]
      }
     }
-    logger.info(s"[Fixed]")
+
+    logger.info(s"The statistics of fixed code: ")
     get_statistics(fixedTokens)
 
     val allTokens = buggyTokens.flatMap(_.toSeq).union(fixedTokens.flatMap(_.toSeq)).filter(_ != "DummyClass")
@@ -71,7 +89,7 @@ class Vocabulary(config: ConfigNamespace) extends parser.JavaParser {
     val buggy_path = config.getString("buggy_path")
     val fixed_path = config.getString("fixed_path")
     val is_abstract = config.getBoolean("is_abstract")
-    val top_k = config.getInt("top_k")
+    val top_k = config.getString("top_k").toInt
 
     val (buggy_files, fixed_files) = if (is_abstract)
       (readFile(buggy_path), readFile(fixed_path))
@@ -85,5 +103,4 @@ class Vocabulary(config: ConfigNamespace) extends parser.JavaParser {
 
     if (top_k < idioms.size) idioms.take(top_k) else idioms
   }
-
 }
